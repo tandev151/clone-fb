e2975cb547232c0a6d9028239c4d17b7a0fbb98c
Tan Lee <Tan Lee>
2023-12-21 19:10:41 +0700

    create theme for proj

---
+++ src/containers/Auth/index.tsx
@@ -0,0 +1,7 @@
+import React, { useContext } from 'react';
+import { ThemeContextType } from '../../actions/type';
+export const AuthContainer = (props: unknown) => {
+  console.log({ props });
+  //   const theme = useContext(props?.ThemeContext);
+  return <div>AuthContainer</div>;
+};
